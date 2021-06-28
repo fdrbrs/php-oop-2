5f042b9669cbab4efc7e2fbbe3ec25ba7fe646cd
fdrbrs <federio.borsci@gmail.com>
2021-06-28 17:58:46 +0200

    made some basic parent and child classes

---
+++ index.php
@@ -0,0 +1,58 @@
+<!-- 
+    Oggi pomeriggio provate ad immaginare quali sono le classi necessarie per creare uno shop online; ad esempio, ci saranno sicuramente dei prodotti da acquistare e degli utenti che fanno shopping.
+Strutturare le classi gestendo l'ereditarietà dove necessario; ad esempio ci potrebbero essere degli utenti premium che hanno diritto a degli sconti esclusivi, oppure diverse tipologie di prodotti.
+Provate a far interagire tra di loro gli oggetti: ad esempio, l'utente dello shop inserisce una carta di credito...
+$c = new CreditCard(..); 
+$user->insertCreditCard($c);
+ -->
+
+ <?php
+
+class User{
+    public $username;
+    public $email;
+    public $age;
+    public $address;
+
+}
+
+class PaymentMethod {
+    protected $name;
+    protected $lastname;
+    protected $billAddress;
+
+}
+
+class CrediCard extends PaymentMethod {
+    protected $cardNumber;
+}
+
+class PayPal extends PaymentMethod {
+    protected $paypalMail;
+}
+
+
+
+class Product{
+    public $name;
+    public $desc;
+    public $price;
+    public $discount;
+    public $manufacturer;
+}
+
+class Book extends Product{
+    public $author;
+}
+
+class Tshirt extends Product{
+    public $size;
+    public $color;
+    
+}
+
+
+
+
+ 
+ ?>
